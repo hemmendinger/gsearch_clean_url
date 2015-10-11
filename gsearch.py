910e19a70d158109b48df918ce72e68c068af8db
Hemmendinger <github@hemmendinger.net>
2015-10-11 17:25:23 -0400

    done

--- gsearch.py
+++ gsearch.py
@@ -3,9 +3,13 @@ from urllib import parse
 from sys import argv
 
 input_url_str = str(argv[1])
+print('Cleaning URL:\n', input_url_str, '\n')
 
+# Get starting index of URL: &url=
+url_start = 5 + input_url_str.find('&url=')
+# Get ending index of URL: &usg
+url_end = input_url_str.find('&usg')
 
+clean_url = parse.unquote_plus(input_url_str[url_start:url_end])
 
-
-
-
+print('Cleaned url:\n', clean_url)
