f57672bb03f6347975d3dd6b2c402b7e43368b74
Hemmendinger <github@hemmendinger.net>
2015-10-12 22:08:01 -0400

    adjust comments

--- gsearch.py
+++ gsearch.py
@@ -5,10 +5,12 @@ from sys import argv
 
 def gsearch_clean_url(input_url_str):
     print('Cleaning URL:\n', input_url_str, '\n')
-    # Get starting index of URL: &url=
+
+    # Start of URL: &url=
+    # End of URL: &usg
     url_start = 5 + input_url_str.find('&url=')
-    # Get ending index of URL: &usg
     url_end = input_url_str.find('&usg')
+
     clean_url = parse.unquote_plus(input_url_str[url_start:url_end])
     print('Cleaned url:\n', clean_url)
 
